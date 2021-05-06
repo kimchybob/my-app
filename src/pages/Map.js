4241bc4a675481bff636b31dbbd66bcc35ce9f5c
kimchybob <38003985+kimchybob@users.noreply.github.com>
2021-05-06 22:46:38 +1000

    initial_version

---
+++ src/pages/Map.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+ 
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+ 
+class Map extends Component {
+  static defaultProps = {
+    center: {
+      lat: -37.945,
+      lng: 145.35
+    },
+    zoom: 11
+  };
+ 
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyChuSnTDJFu49qaND1aweNAZ57OOipEqKk" }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          {/* <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          /> */}
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+ 
+export default Map;
